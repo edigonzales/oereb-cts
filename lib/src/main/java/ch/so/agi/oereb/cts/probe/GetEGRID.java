958352d9e317b147f1c73f8d3aa77cbae4949fe6
edigonzales <edi.gonzales@gmail.com>
2022-09-13 11:52:55 +0200

    prototyping

---
+++ lib/src/main/java/ch/so/agi/oereb/cts/probe/GetEGRID.java
@@ -0,0 +1,53 @@
+package ch.so.agi.oereb.cts.probe;
+
+import java.util.HashMap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class GetEGRID /* extends? implements? */ {
+    final Logger log = LoggerFactory.getLogger(GetEGRID.class);
+
+    private String requestTemplate = "";
+    
+    private String result;
+    
+    public void run(String serviceEndpoint, HashMap<String,String> parameters) {
+        
+        
+        
+        // Loop 1: Varianten
+        
+            // Loop 2: Query parameter
+                
+                // 1. make request
+                
+                
+                // 2. validate response code
+                
+                
+                // 3. validate schema
+                
+                
+                // 4. custom validations 
+        
+        
+    }
+    
+    // mutterklasse?
+    private void makeRequest() {
+        
+    }
+    
+    // mutterklasse?
+    private void validateSchema(String schema, String result) {
+        
+    }
+    
+    // mutterklasse?
+    private void validateResponseCode(String responseCode, String result) {
+        
+    }
+    
+}
