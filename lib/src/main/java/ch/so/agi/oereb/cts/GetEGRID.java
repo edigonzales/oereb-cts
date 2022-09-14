97b4a680e226792effb5da61a6d40ecd6b360092
edigonzales <edi.gonzales@gmail.com>
2022-09-14 17:59:16 +0200

    wip

--- lib/src/main/java/ch/so/agi/oereb/cts/GetEGRID.java
+++ lib/src/main/java/ch/so/agi/oereb/cts/GetEGRID.java
@@ -1,7 +1,14 @@
 package ch.so.agi.oereb.cts;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.URI;
 import java.net.URLDecoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpClient.Redirect;
+import java.net.http.HttpClient.Version;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.List;
@@ -11,6 +18,11 @@ import org.apache.commons.text.StringSubstitutor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
+
+// TODO:
+// falls nicht alle Parameter geliefert werden, wird diese Variante nicht ausgeführt.
+// Z.B. nicht alle negativen Tests testen, d.h. Koordinate ausserhalb für 204er
 
 public class GetEGRID /* extends? implements? */ {
     final Logger log = LoggerFactory.getLogger(GetEGRID.class);
@@ -31,9 +43,19 @@ public class GetEGRID /* extends? implements? */ {
     // Result Rückgabewert?
     public void run(String serviceEndpoint, Map<String,String> parameters) {
         
+
+        
+        
         for (var requestTemplate : requestTemplates) {
             var subsitutor = new StringSubstitutor(parameters);
-            var resolvedRequestTemplate = subsitutor.replace(requestTemplate);
+            String resolvedRequestTemplate = null;
+            try {
+                resolvedRequestTemplate = subsitutor.replace(requestTemplate);
+            } catch (IllegalArgumentException e) {
+                log.info("FUUUUUUUUUUUUUUUUUUUUUUUUUUUUU");
+                continue;
+            }
+            subsitutor.setEnableUndefinedVariableException(true);
             log.info(resolvedRequestTemplate);
             
             try {
@@ -44,25 +66,28 @@ public class GetEGRID /* extends? implements? */ {
                     requestUrl += queryParameter;
                     log.info(requestUrl);
                     
-                    
+
+                    var requestBuilder = HttpRequest.newBuilder();
+                    requestBuilder.GET().uri(URI.create(requestUrl));
+
+                    var request = requestBuilder.build();
+                    var httpClient = this.createHttpClient();
+                    var response = httpClient.send(request, HttpResponse.BodyHandlers.ofInputStream());
+
+                    log.info("status code:" + response.statusCode());
                     
                     
                     
                 }
-                
-                
-                
-                
-                
 
             } catch (UnsupportedEncodingException e) {
                 // TODO result...
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
- 
 
-            
-            
-            
         }
         
         // Loop 1: Varianten
@@ -81,6 +106,15 @@ public class GetEGRID /* extends? implements? */ {
                 // 4. custom validations 
         
         
+    }
+    
+    // mutterklasse
+    private HttpClient createHttpClient() {
+        var httpClient = HttpClient.newBuilder()
+                .version(Version.HTTP_1_1)
+                .followRedirects(Redirect.ALWAYS)
+                .build();
+        return httpClient;
     }
     
     // mutterklasse?
--- lib/src/test/java/ch/so/agi/oereb/cts/GetEGRIDTest.java
+++ lib/src/test/java/ch/so/agi/oereb/cts/GetEGRIDTest.java
@@ -7,7 +7,8 @@ import org.junit.jupiter.api.Test;
 public class GetEGRIDTest {
     @Test
     public void foo() {
-        var parameters = Map.of("EN","2600595,1215629","IDENTDN","SO0200002457","NUMBER","168");
+        //var parameters = Map.of("EN","2600595,1215629","IDENTDN","SO0200002457","NUMBER","168");
+        var parameters = Map.of("EN","2600595,1215629");
         var getEgrid = new GetEGRID();
         getEgrid.run("https://geo.so.ch/api/oereb/", parameters);
         
